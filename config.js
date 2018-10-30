7a85123d2ce89fa8820ab7cf60bee8a82bb367ce
Brad Traversy <traversymedia@gmail.com>
2018-10-30 17:17:00 -0400

    Edited Config

--- config.js
+++ config.js
@@ -2,8 +2,6 @@ module.exports = {
   ENV: process.env.NODE_ENV || 'development',
   PORT: process.env.PORT || 3000,
   URL: process.env.BASE_URL || 'http://localhost:3000',
-  MONGODB_URI:
-    process.env.MONGODB_URI ||
-    'mongodb://abc123!:abc123!@ds145573.mlab.com:45573/customer_api',
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://YOUR_URI',
   JWT_SECRET: process.env.JWT_SECRET || 'secret1'
 };
